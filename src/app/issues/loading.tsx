118a08b4c0552d6ca61b039750d86cc5ee01e8f3
MarcelOlsen <marcelolsen26@gmail.com>
2024-01-27 15:38:35 +0100

    :children_crossing: Added loading skeletons
    Added loading skeletons to improve user experience with slow connections

--- src/app/issues/loading.tsx
+++ src/app/issues/loading.tsx
@@ -1,19 +1,11 @@
-"use client";
-
 import React from "react";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
-import prisma from "@/prisma/client";
-import IssueStatusBadge from "../components/IssueStatusBadge";
 import IssueActions from "./IssueActions";
-import delay from "delay";
+import { Table } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
-
-  // Simulate a slow API call
-  await delay(2000);
-
+const IssuePageLoading = () => {
+  const issues = [1, 2, 3, 4, 5];
   return (
     <div>
       <IssueActions />
@@ -26,18 +18,18 @@ const IssuesPage = async () => {
           </Table.Row>
           <Table.Body>
             {issues.map((issue) => (
-              <Table.Row key={issue.id}>
+              <Table.Row key={issue}>
                 <Table.Cell>
-                  <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                  <Skeleton />
                   <div className="block md:hidden">
-                    <IssueStatusBadge status={issue.status} />
+                    <Skeleton />
                   </div>
                 </Table.Cell>
                 <Table.Cell className="hidden md:table-cell">
-                  <IssueStatusBadge status={issue.status} />
+                  <Skeleton />
                 </Table.Cell>
                 <Table.Cell className="hidden md:table-cell">
-                  {issue.createdAt.toDateString()}
+                  <Skeleton />
                 </Table.Cell>
               </Table.Row>
             ))}
@@ -48,4 +40,4 @@ const IssuesPage = async () => {
   );
 };
 
-export default IssuesPage;
+export default IssuePageLoading;
